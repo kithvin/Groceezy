3e5bab0fd46a79560a5be823719bdcf9e1a223ba
kithvin <kithvinbandara1994@gmai.com>
2025-06-20 19:12:45 +0530

    Added best seller product section with dynamic product cards and cart controls

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import { Route, Routes, useLocation } from "react-router-dom";
 import Contact from "./pages/Contact";
 import Home from "./pages/Home";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
 
@@ -11,6 +12,7 @@ const App = () => {
   return (
     <div>
     {isSellerPath ? null : <Navbar /> } 
+    <Toaster/>
       <div className={`${isSellerPath ? "" : "px-6 md:px-16 lg:px-24 xl:px-32"}`}>
         <Routes>
           <Route path="/" element={<Home/>} />
---
+++ Frontend/src/components/BestSeller.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import ProductCard from "./ProductCard";
+import { useAppContext } from "../context/AppContext";
+
+// BestSeller section
+function BestSeller() {
+  const { products } = useAppContext(); // Get products from context
+  return (
+    <div className="mt-16 px-4 md:px-8 lg:px-20">
+      
+      {/* Section title */}
+      <h2 className="text-2xl md:text-3xl font-semibold text-center mb-10">
+        Best Sellers
+      </h2>
+
+    {/* Product grid */}
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 gap-6 xl:grid-cols-5">
+        {products
+          .filter((product) => product.inStock) // Only in-stock products
+          .slice(0, 10)
+          .map((product, index) => (
+            <div key={index} className="flex justify-center">
+              <ProductCard product={product} /> {/* Show product card */}
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+}
+
+export default BestSeller;
