d5ccf4e2e1d117c8af15f1de472ade5165735b5a
kithvin <kithvinbandara1994@gmai.com>
2025-06-21 08:36:28 +0530

    Added some features in BestSeller component

--- Frontend/src/components/BestSeller.jsx
+++ Frontend/src/components/BestSeller.jsx
@@ -1,31 +1,54 @@
-import React from "react";
-import ProductCard from "./ProductCard";
-import { useAppContext } from "../context/AppContext";
+// import React from "react";
+// import ProductCard from "./ProductCard";
+// import { useAppContext } from "../context/AppContext";
 
-// BestSeller section
-function BestSeller() {
+// // BestSeller section
+// function BestSeller() {
+//   const { products } = useAppContext(); // Get products from context
+//   return (
+//     <div className="mt-16 px-4 md:px-8 lg:px-20">
+      
+//       {/* Section title */}
+//       <h2 className="text-2xl md:text-3xl font-semibold text-center mb-10">
+//         Best Sellers
+//       </h2>
+
+//     {/* Product grid */}
+//       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 gap-6 xl:grid-cols-5">
+//         {products
+//           .filter((product) => product.inStock) // Only in-stock products
+//           .slice(0, 10)
+//           .map((product, index) => (
+//             <div key={index} className="flex justify-center">
+//               <ProductCard product={product} /> {/* Show product card */}
+//             </div>
+//           ))}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default BestSeller;
+
+import React from 'react'
+import ProductCard from './ProductCard'
+import { useAppContext } from '../context/AppContext'
+
+const BestSeller = () => {
   const { products } = useAppContext(); // Get products from context
   return (
-    <div className="mt-16 px-4 md:px-8 lg:px-20">
-      
-      {/* Section title */}
-      <h2 className="text-2xl md:text-3xl font-semibold text-center mb-10">
-        Best Sellers
-      </h2>
-
-    {/* Product grid */}
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 gap-6 xl:grid-cols-5">
-        {products
-          .filter((product) => product.inStock) // Only in-stock products
-          .slice(0, 10)
-          .map((product, index) => (
-            <div key={index} className="flex justify-center">
-              <ProductCard product={product} /> {/* Show product card */}
-            </div>
-          ))}
-      </div>
+    <div className='mt-16 px-4 md:px-8 lg:px-20'>
+    <p className='text-2xl md:text-3xl font-semibold text-center'>Best Seller</p>
+    <div className='grid  sm:grid-cols-3 md:grid-cols-4 gap-3 md:gap-6 lg:grid-cols-5 mt-6 justify-center'>
+      {products.filter((product) => product.inStock) 
+      .slice(0, 5).map((product, index)=>(
+        <ProductCard key={index} product={product}/>
+      ))
+      }
+    
     </div>
-  );
+    </div>
+  )
 }
 
-export default BestSeller;
+export default BestSeller
