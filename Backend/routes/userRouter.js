eeaca658b06c444304cc2d703582fd62a1ca0c3a
kithvin <kithvinbandara1994@gmai.com>
2025-07-04 10:25:32 +0530

    Added user authentication check and logout functionality using JWT middleware

--- Backend/routes/userRouter.js
+++ Backend/routes/userRouter.js
@@ -1,6 +1,7 @@
 // Import required modules
 import express from "express"; // Import Express framework
-import { Register, login } from "../controllers/userController.js"; // Controller function
+import { Register, isAuth, login, logout } from "../controllers/userController.js"; // Controller function
+import authUser from '../middlewares/authUser.js'
 
 // Create a new Express Router instance
 const userRouter = express.Router();
@@ -12,6 +13,14 @@ userRouter.post("/register", Register);
 // Login a user
 // POST /api/user/login
 userRouter.post("/login", login);
+
+// Check if user is authenticated (protected route)
+// GET /api/user/is-auth
+userRouter.get("/is-auth", authUser, isAuth);
+
+// Logout user (protected route)
+// GET /api/user/logout
+userRouter.get("/logout", authUser, logout);
 
 // Export the router to use in the main server file
 export default userRouter;
