6f269d1ce6ebf1ce6eb2855fb60312276b003845
kithvinbandara1994@gmail.com <kithvinbandara1994@gmai.com>
2025-07-05 18:23:58 +0530

    Fetched user addresses from backend in cart page using /api/address/get

--- Backend/models/Address.js
+++ Backend/models/Address.js
@@ -3,16 +3,16 @@ import mongoose from "mongoose";
 
 // Define the address schema for storing address data
 const addressSchema = new mongoose.Schema({
-  userId: { type: String, require: true }, // The user ID associated with the address
-  firstName: { type: String, require: true }, // First name of the address holder
-  lastName: { type: String, require: true }, // Last name of the address holder
-  email: { type: String, require: true }, // Email address of the address holder
-  street: { type: String, require: true }, // Street address
-  city: { type: String, require: true }, // City of the address
-  state: { type: String, require: true }, // State of the address
-  zipcode: { type: Number, require: true }, // Zip code of the address
-  country: { type: String, require: true }, // Country of the address
-  phone: { type: String, require: true }, // Phone number of the address holder
+  userId: { type: String, required: true }, // The user ID associated with the address
+  firstName: { type: String, required: true }, // First name of the address holder
+  lastName: { type: String, required: true }, // Last name of the address holder
+  email: { type: String, required: true }, // Email address of the address holder
+  street: { type: String, required: true }, // Street address
+  city: { type: String, required: true }, // City of the address
+  state: { type: String, required: true }, // State of the address
+  zipcode: { type: Number, required: true }, // Zip code of the address
+  country: { type: String, required: true }, // Country of the address
+  phone: { type: String, required: true }, // Phone number of the address holder
 });
 
 // Create the Address model if it doesn't already exist in mongoose
--- Frontend/src/pages/AddAddress.jsx
+++ Frontend/src/pages/AddAddress.jsx
@@ -1,5 +1,7 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { assets } from "../assets/assets";
+import { useAppContext } from "../context/AppContext";
+import toast from "react-hot-toast";
 
 // Reusable InputField component for form inputs
 const InputField = ({ type, placeholder, name, handleChange, address }) => (
@@ -13,6 +15,9 @@ const InputField = ({ type, placeholder, name, handleChange, address }) => (
   />
 );
 const AddAddress = () => {
+
+  const {axios, user, navigate} = useAppContext();
+
   // State to store address input values
   const [address, setAddress] = useState({
     firstName: "",
@@ -34,12 +39,30 @@ const AddAddress = () => {
       ...prevAddress,
       [name]: value,
     }));
+    console.log(address)
   };
 
   // Form submit handler
   const onSubmitHandler = async (e) => {
     e.preventDefault();
+    try {
+      const {data} = await axios.post('/api/address/add', {address});
+      if (data.success){
+        toast.success(data.message);
+        navigate('/cart')
+      }else{
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
   };
+
+  useEffect(()=>{
+      if(!user){
+        navigate('/cart');
+      }
+  },[])
 
   return (
     <div className="mt-16 pb-16 ml-12">
